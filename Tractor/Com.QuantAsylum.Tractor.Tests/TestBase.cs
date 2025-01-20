f6b823af6e1a685579c2e6ba88ca0e33f0c066a7
Tom Kuzma <77029857+tomkuzma@users.noreply.github.com>
2025-01-20 14:05:36 -0800

    added phase180 test

--- Tractor/Com.QuantAsylum.Tractor.Tests/TestBase.cs
+++ Tractor/Com.QuantAsylum.Tractor.Tests/TestBase.cs
@@ -91,7 +91,8 @@ namespace Com.QuantAsylum.Tractor.Tests
     [System.Xml.Serialization.XmlInclude(typeof(SumGainA01))]
     [System.Xml.Serialization.XmlInclude(typeof(SplitGainA01))]
     [System.Xml.Serialization.XmlInclude(typeof(GainSelectA01))]
-    [System.Xml.Serialization.XmlInclude(typeof(LRBalanceA01))]
+    [System.Xml.Serialization.XmlInclude(typeof(LRBalanceA01))] 
+    [System.Xml.Serialization.XmlInclude(typeof(PhaseTest180A01))]
 
     //
     // Naming Convention for classes:
